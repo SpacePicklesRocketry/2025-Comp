3071826ea20c874f0065a74d44081ab540383c75
Nikhil <namnik100@gmail.com>
2024-12-21 16:56:19 -0800

    filtering

--- src/main/Sensors.cpp
+++ src/main/Sensors.cpp
@@ -10,6 +10,11 @@
 #define SEALEVELPRESSURE_HPA 1013.25 // Standard sea-level pressure in hPa
 #define APOGEE_THRESHOLD -0.5 // Apogee detection threshold: Negative rate of climb
 
+// Constants for velocity and motion
+#define ACCEL_THRESHOLD 0.1  // Threshold for motion detection
+#define RESET_THRESHOLD 0.05 // Velocity threshold to reset on stationary
+#define DRAG_FACTOR 0.98     // Drag factor for velocity decay
+
 // Instances for BMP sensors
 Adafruit_BMP3XX bmp1;
 Adafruit_BMP3XX bmp2;
@@ -19,7 +24,7 @@ SimpleKalmanFilter altitudeFilter(2.0, 2.0, 0.5); // Measurement error, Estimate
 
 // Variables for altitude and rate of climb
 float previousAltitude = 0;
-float currentAltitude = 0;
+float accelBaselineX = 0, accelBaselineY = 0, accelBaselineZ = GRAVITY;
 
 void initializeSensors() {
     if (!IMU.begin()) {
@@ -73,7 +78,6 @@ SensorData readSensors(float deltaTime, SensorData& previousData) {
     // Read gyroscope data
     if (IMU.gyroscopeAvailable()) {
         IMU.readGyroscope(gyroX_raw, gyroY_raw, gyroZ_raw);
-        
 
         // Convert gyroscope readings to degrees per second
         data.gyroX = gyroX_raw * 180.0 / M_PI;
@@ -90,25 +94,46 @@ SensorData readSensors(float deltaTime, SensorData& previousData) {
     if (IMU.accelerationAvailable()) {
         IMU.readAcceleration(accelX_raw, accelY_raw, accelZ_raw);
 
-        // Adjust raw accelerometer readings
-        accelX_raw *= GRAVITY;
-        accelY_raw *= GRAVITY;
-        accelZ_raw *= GRAVITY;
+        // Subtract baseline and apply threshold
+        float accelX_filtered = accelX_raw - accelBaselineX;
+        float accelY_filtered = accelY_raw - accelBaselineY;
+        float accelZ_filtered = accelZ_raw - accelBaselineZ;
 
-        // Direct assignment to data fields
-        data.accelX = accelX_raw;
-        data.accelY = accelY_raw;
-        data.accelZ = accelZ_raw;
+        if (fabs(accelX_filtered) < ACCEL_THRESHOLD) accelX_filtered = 0;
+        if (fabs(accelY_filtered) < ACCEL_THRESHOLD) accelY_filtered = 0;
+        if (fabs(accelZ_filtered) < ACCEL_THRESHOLD) accelZ_filtered = 0;
 
-        // Integrate acceleration to calculate velocity
-        data.velocityX = previousData.velocityX + data.accelX * deltaTime;
-        data.velocityY = previousData.velocityY + data.accelY * deltaTime;
-        data.velocityZ = previousData.velocityZ + data.accelZ * deltaTime;
+        // Update data fields
+        data.accelX = accelX_filtered * GRAVITY;
+        data.accelY = accelY_filtered * GRAVITY;
+        data.accelZ = accelZ_filtered * GRAVITY;
+
+        // Integrate acceleration to velocity
+        if (fabs(data.accelX) > RESET_THRESHOLD || fabs(data.accelY) > RESET_THRESHOLD || fabs(data.accelZ) > RESET_THRESHOLD) {
+            data.velocityX = previousData.velocityX + data.accelX * deltaTime;
+            data.velocityY = previousData.velocityY + data.accelY * deltaTime;
+            data.velocityZ = previousData.velocityZ + data.accelZ * deltaTime;
+        } else {
+            data.velocityX = 0;
+            data.velocityY = 0;
+            data.velocityZ = 0;
+        }
+
+        // Apply drag to reduce drift
+        data.velocityX *= DRAG_FACTOR;
+        data.velocityY *= DRAG_FACTOR;
+        data.velocityZ *= DRAG_FACTOR;
 
         // Integrate velocity to calculate position
         data.positionX = previousData.positionX + data.velocityX * deltaTime;
         data.positionY = previousData.positionY + data.velocityY * deltaTime;
         data.positionZ = previousData.positionZ + data.velocityZ * deltaTime;
+
+        // Debugging output
+        Serial.print("Velocity X: ");
+        Serial.println(data.velocityX);
+        Serial.print("Position X: ");
+        Serial.println(data.positionX);
     }
 
     // Read altitude from BMP sensor
@@ -121,22 +146,14 @@ SensorData readSensors(float deltaTime, SensorData& previousData) {
         Serial.println("Apogee detected!");
     }
 
-    // Update previous altitude for the next iteration
+    // Update previous altitude
     previousAltitude = data.altitude;
-
-    // Debugging output
-    // Serial.print("Altitude: ");
-    // Serial.print(data.altitude);
-    // Serial.print(" m, Rate of Climb: ");
-    // Serial.println(data.rateOfChange);
 
     data.timestamp = millis();
     return data;
 }
 
-
 float readAltitudeFromBMP() {
-    // Check both sensors and use the one that is available
     if (bmp1.performReading()) {
         return bmp1.readAltitude(SEALEVELPRESSURE_HPA);
     } else if (bmp2.performReading()) {
--- src/main/main.ino
+++ src/main/main.ino
@@ -5,9 +5,11 @@
 SensorData previousData = {};
 
 void setup() {
+    // Initialize serial communication
     Serial.begin(115200);
     while (!Serial);
 
+    // Initialize sensors and SD card
     initializeSensors();
     initializeSDCard();
     createLogFile();
@@ -16,25 +18,39 @@ void setup() {
 }
 
 void loop() {
-    static unsigned long lastTime = millis();
+    // Delta time calculation
+    static unsigned long lastTime = 0;
+    if (lastTime == 0) lastTime = millis(); // Initialize on first iteration
+
     unsigned long currentTime = millis();
-    float deltaTime = (currentTime - lastTime) / 1000.0;
+    float deltaTime = (currentTime - lastTime) * 0.001; // Convert to seconds
     lastTime = currentTime;
 
+    // Read sensors and log data
     SensorData currentData = readSensors(deltaTime, previousData);
     logData(currentData);
 
-// Serial.print("Accel (m/s^2): ");
-// Serial.print("X: "); Serial.print(currentData.accelX);
-// Serial.print(", Y: "); Serial.print(currentData.accelY);
-// Serial.print(", Z: "); Serial.println(currentData.accelZ);
+    // Debugging output
+    Serial.print("Delta Time (s): ");
+    Serial.println(deltaTime);
 
+    Serial.print("Accel (m/s^2): ");
+    Serial.print("X: "); Serial.print(currentData.accelX);
+    Serial.print(", Y: "); Serial.print(currentData.accelY);
+    Serial.print(", Z: "); Serial.println(currentData.accelZ);
 
-Serial.print("Gyro (deg/s): ");
-Serial.print("X: "); Serial.print(currentData.gyroX);
-Serial.print(", Y: "); Serial.print(currentData.gyroY);
-Serial.print(", Z: "); Serial.println(currentData.gyroZ);
+    Serial.print("Gyro (deg/s): ");
+    Serial.print("X: "); Serial.print(currentData.gyroX);
+    Serial.print(", Y: "); Serial.print(currentData.gyroY);
+    Serial.print(", Z: "); Serial.println(currentData.gyroZ);
 
+    Serial.print("Position (m): ");
+    Serial.print("X: "); Serial.print(currentData.positionX);
+    Serial.print(", Y: "); Serial.print(currentData.positionY);
+    Serial.print(", Z: "); Serial.println(currentData.positionZ);
+
+    // Update previous data for the next loop
     previousData = currentData;
-    delay(100);
+
+    delay(100); // Small delay for stability
 }
