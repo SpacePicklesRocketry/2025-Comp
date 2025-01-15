c4a57596f08a02a87b614eaeebdc939cd1ee06e0
vardam <vardam108@gmail.com>
2025-01-14 16:10:04 -0800

    Adjusted angle values. Still need proper angles.

--- src/main/airbrake.cpp
+++ src/main/airbrake.cpp
@@ -9,29 +9,29 @@ airbrake1_pin = 8; //CHANGE TO CORRECT PIN!!!
 airbrake2_pin = 9; //CHANGE TO CORRECT PIN!!!
 alt_threshold = 182.88; //Meters - CHANGE TO CORRECT THRESHOLD!!! 182.88 meters = 600 feet
 airbrake_deployed = false;
-airbrake_open = 2500;
-airbrake_close = 500;
+airbrake_open = 150; //CHANGE TO CORRECT ANGLE!!!
+airbrake_close = -3; //CHANGE TO CORRECT ANGLE!!!
 
 void setup(){
     servo1.attach(airbrake1_pin);
     servo2.attach(airbrake2_pin);
     initializeSensors();
-    servo1.writeMicroseconds(airbrake_close);
-    servo2.writeMicroseconds(airbrake_close);
+    servo1.write(airbrake_close);
+    servo2.write(airbrake_close);
 }
 
 void openAirbrake(bool &airbrake_deployed){
     if (readAltitudeFromBMP() >= alt_threshold && airbrake_deployed == false){
-        servo1.writeMicroseconds(airbrake_open);
-        servo2.writeMicroseconds(airbrake_open);
+        servo1.write(airbrake_open);
+        servo2.write(airbrake_open);
         airbrake_deployed = true;
     }
 }
 
 void closeAirbrake(bool &airbrake_deployed){
     if (readAltitudeFromBMP() < alt_threshold && airbrake_deployed == true){
-        servo1.writeMicroseconds(airbrake_close);
-        servo2.writeMicroseconds(airbrake_close);
+        servo1.write(airbrake_close);
+        servo2.write(airbrake_close);
         airbrake_deployed = false;
     }
 }
